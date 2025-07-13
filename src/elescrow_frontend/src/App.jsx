b6158a95be8962bb0c6480ef8024c659081000be
aurelius.wijaya@binus.ac.id <zelirius@DESKTOP-K9478LT.>
2025-07-13 23:14:51 +0700

    change backend to rust

---
+++ src/elescrow_frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { elescrow_backend } from "declarations/elescrow_backend";
+
+function App() {
+  const [greeting, setGreeting] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    elescrow_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+
+      <form action="#" onSubmit={handleSubmit}>
+        <h1 className="text-3xl font-bold text-blue-500">
+          Hello Tailwind v3 🎈
+        </h1>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
